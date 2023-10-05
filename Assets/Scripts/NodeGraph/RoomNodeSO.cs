f2660f34201bc3aeefe4819cef27885eb207aff5
Ring <85309246+Pinochio195@users.noreply.github.com>
2023-10-06 00:12:05 +0700

    new update code

--- Assets/Scripts/NodeGraph/RoomNodeSO.cs
+++ Assets/Scripts/NodeGraph/RoomNodeSO.cs
@@ -174,7 +174,7 @@ public class RoomNodeSO : ScriptableObject
         GUI.changed = true;
     }
 
-    private void DragNode(Vector2 delta)
+    public void DragNode(Vector2 delta)
     {
         rect.position += delta;
         EditorUtility.SetDirty(this);
